2fad502cfa2f1c7c0cce293a546749afe8043972
oat9002 <oat9002@hotmail.com>
2024-07-31 21:33:59 +0700

    🚧

---
+++ src/lib/component/AddTransaction.svelte
@@ -0,0 +1,3 @@
+<div>
+    <button class="btn">Start</button>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import type { User } from "$lib/model/User";
 	import { addUser } from "$lib/service/database";
+    import AddTransaction from "$lib/component/AddTransaction.svelte";
 
     export let data: { users: User[] };  
     let userName: string = "";  
@@ -19,6 +20,10 @@
 <div>
     <input type="text" placeholder="Name" class="input input-bordered w-full max-w-xs" on:change={onUserNameChange} />
     <button class="btn" on:click={() => onUserAddClick()}>Add</button>
+</div>
+<AddTransaction/>
+<div>
+    <button class="btn">+ Add Transaction</button>
 </div>
 
 
